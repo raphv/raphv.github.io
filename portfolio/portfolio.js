da1102f500d3850770cc148b616f1a0acd844838
Raphaël Velt <raphael@velt.info>
2016-08-03 16:35:23 +0100

    Portfolio v1

---
+++ portfolio/portfolio.js
@@ -0,0 +1,48 @@
+$(function() {
+    
+    var currentLanguage = "en";
+    var availableLanguages = $('li.lang').map(function(){return this.lang;}).toArray();
+    var browserLanguages = window.navigator.languages || [window.navigator.language || window.navigator.userLanguage || "en"];
+    for (var i = 0; i < browserLanguages.length; i++) {
+        var langToTest = browserLanguages[i];
+        if (availableLanguages.indexOf(langToTest) !== -1) {
+            currentLanguage = langToTest;
+            break;
+        }
+    }
+    
+    function refreshLanguage() {
+        $(".multilang li[lang]").addClass("inactive-language");
+        $(".multilang li[lang='" + currentLanguage + "']").removeClass("inactive-language");
+        $("ul.multilang").each(function() {
+            var $children = $(this).find("li[lang]");
+            if ($children.length === $children.filter(".inactive-language").length) {
+                $children.first().removeClass("inactive-language");
+            }
+        });
+        $("#langselect li").removeClass("active");
+        $("#langselect li[lang='" + currentLanguage + "']").addClass("active");
+        $("title").text($("h1 .multilang li[lang='" + currentLanguage + "']").text());
+    }
+    
+    $("#langselect li").click(function() {
+        currentLanguage = this.lang;
+        refreshLanguage();
+        return false;
+    });
+    
+    var $w = $(window);
+    
+    $("div.project-image, h3.project-title").click(function() {
+        var $p = $(this).parent();
+        var isdet = $p.hasClass("show-detail");
+        $("li.project").removeClass("show-detail");
+        $p.toggleClass("show-detail",!isdet);
+        $('html, body').animate({
+            'scrollTop': ($p.offset().top + $p.height()/2 - $w.height() / 2)
+        }, 500);
+        return false;
+    });
+    
+    refreshLanguage();
+});
